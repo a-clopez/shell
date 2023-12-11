4ddac0fead624211723eca02e4ea01161d5067f9
a-clopez <a.clopez@udc.es>
2023-12-11 16:49:24 +0100

    help code

--- p2.c
+++ p2.c
@@ -1148,240 +1148,155 @@ void Cmd_memdump(char *tr[], int num_args) {
     }
 }
 /* Help code:
-  lo que esta en comentarios con 2
-depende de las funciones que se hayan hecho para la lista
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/utsname.h>
-#include <sys/stat.h>
-#include <stdbool.h>
-#include <ctype.h>
-#include <fcntl.h>
-#include <pwd.h>
-#include <grp.h>
-#include <dirent.h>
+*el siguiente codigo se da como ayuda por si se quiere utilizar
+NO ES OBLIGATORIO USARLO
+y pueden usarse funciones enteras o parte de funciones
 
-#define TAMANO 2048
+Este fichero, ayudaP3.c no está pensado para ser compilado separadamente
+, entre otras cosas, no contiene los includes necesarios
+y las constantes utilizadas, no están definidas en él
 
-void Recursiva (int n)
+void Cmd_fork (char *tr[])
 {
-    char automatico[TAMANO];
-    static char estatico[TAMANO];
+    pid_t pid;
 
-    printf ("parametro:%3d(%p) array %p, arr estatico %p\n",n,&n,automatico, estatico);
-
-    if (n>0)
-        Recursiva(n-1);
-}
-
-
-void LlenarMemoria (void *p, size_t cont, unsigned char byte)
-{
-    unsigned char *arr=(unsigned char *) p;
-    size_t i;
-
-    for (i=0; i<cont;i++)
-        arr[i]=byte;
-}
-
-void * ObtenerMemoriaShmget (key_t clave, size_t tam)
-{
-    void * p;
-    int aux,id,flags=0777;
-    struct shmid_ds s;
-
-    if (tam)     //tam distito de 0 indica crear
-        flags=flags | IPC_CREAT | IPC_EXCL;
-    if (clave==IPC_PRIVATE)  //no nos vale
-    {errno=EINVAL; return NULL;}
-    if ((id=shmget(clave, tam, flags))==-1)
-        return (NULL);
-    if ((p=shmat(id,NULL,0))==(void*) -1){
-        aux=errno;
-        if (tam)
-            shmctl(id,IPC_RMID,NULL);
-        errno=aux;
-        return (NULL);
+    if ((pid=fork())==0){
+		VaciarListaProcesos(&LP); Depende de la implementación de cada uno
+        printf ("ejecutando proceso %d\n", getpid());
     }
-    shmctl (id,IPC_STAT,&s);
-    /Guardar en la lista, p.e.  InsertarNodoShared (&L, p, s.shm_segsz, clave);
-    return (p);
+    else if (pid!=-1)
+        waitpid (pid,NULL,0);
 }
-void SharedCreate (char *tr[])
+
+int BuscarVariable (char * var, char *e[])  /busca una variable en el entorno que se le pasa como parámetro
 {
-    key_t cl;
-    size_t tam;
-    void *p;
+    int pos=0;
+    char aux[MAXVAR];
 
-    if (tr[0]==NULL || tr[1]==NULL) {
-        / ImprimirListaShared(&L);
-        return;
-    }
+    strcpy (aux,var);
+    strcat (aux,"=");
 
-    cl=(key_t)  strtoul(tr[0],NULL,10);
-    tam=(size_t) strtoul(tr[1],NULL,10);
-    if (tam==0) {
-        printf ("No se asignan bloques de 0 bytes\n");
-        return;
-    }
-    if ((p=ObtenerMemoriaShmget(cl,tam))!=NULL)
-        printf ("Asignados %lu bytes en %p\n",(unsigned long) tam, p);
-    else
-        printf ("Imposible asignar memoria compartida clave %lu:%s\n",(unsigned long) cl,strerror(errno));
+    while (e[pos]!=NULL)
+        if (!strncmp(e[pos],aux,strlen(aux)))
+            return (pos);
+        else
+            pos++;
+    errno=ENOENT;   /*no hay tal variable
+    return(-1);
 }
 
 
-void * MapearFichero (char * fichero, int protection)
-{
-    int df, map=MAP_PRIVATE,modo=O_RDONLY;
-    struct stat s;
-    void *p;
+int CambiarVariable(char * var, char * valor, char *e[]) /cambia una variable en el entorno que se le pasa como parámetro
+{                                                        /lo hace directamente, no usa putenv
+    int pos;
+    char *aux;
 
-    if (protection&PROT_WRITE)
-        modo=O_RDWR;
-    if (stat(fichero,&s)==-1 || (df=open(fichero, modo))==-1)
-        return NULL;
-    if ((p=mmap (NULL,s.st_size, protection,map,df,0))==MAP_FAILED)
-        return NULL;
-/* Guardar en la lista    InsertarNodoMmap (&L,p, s.st_size,df,fichero);
-    return p;
-}
+    if ((pos=BuscarVariable(var,e))==-1)
+        return(-1);
 
-void CmdMmap(char *arg[])
-{
-    char *perm;
-    void *p;
-    int protection=0;
-
-    if (arg[0]==NULL)
-    {ImprimirListaMmap(&L); return;}
-    if ((perm=arg[1])!=NULL && strlen(perm)<4) {
-        if (strchr(perm,'r')!=NULL) protection|=PROT_READ;
-        if (strchr(perm,'w')!=NULL) protection|=PROT_WRITE;
-        if (strchr(perm,'x')!=NULL) protection|=PROT_EXEC;
-    }
-    if ((p=MapearFichero(arg[0],protection))==NULL)
-        perror ("Imposible mapear fichero");
-    else
-        printf ("fichero %s mapeado en %p\n", arg[0], p);
-}
-
-void SharedDelkey (char *args[])
-{
-    key_t clave;
-    int id;
-    char *key=args[0];
-
-    if (key==NULL || (clave=(key_t) strtoul(key,NULL,10))==IPC_PRIVATE){
-        printf ("      delkey necesita clave_valida\n");
-        return;
-    }
-    if ((id=shmget(clave,0,0666))==-1){
-        perror ("shmget: imposible obtener memoria compartida");
-        return;
-    }
-    if (shmctl(id,IPC_RMID,NULL)==-1)
-        perror ("shmctl: imposible eliminar id de memoria compartida\n");
-}
-
-
-ssize_t EscribirFichero (char *f, void *p, size_t cont,int overwrite)
-{
-    ssize_t  n;
-    int df,aux, flags=O_CREAT | O_EXCL | O_WRONLY;
-
-    if (overwrite)
-        flags=O_CREAT | O_WRONLY | O_TRUNC;
-
-    if ((df=open(f,flags,0777))==-1)
+    if ((aux=(char *)malloc(strlen(var)+strlen(valor)+2))==NULL)
         return -1;
-
-    if ((n=write(df,p,cont))==-1){
-        aux=errno;
-        close(df);
-        errno=aux;
-        return -1;
-    }
-    close (df);
-    return n;
+    strcpy(aux,var);
+    strcat(aux,"=");
+    strcat(aux,valor);
+    e[pos]=aux;
+    return (pos);
 }
 
-ssize_t LeerFichero (char *f, void *p, size_t cont)
+
+/las siguientes funciones nos permiten obtener el nombre de una senal a partir
+del número y viceversa
+static struct SEN sigstrnum[]={
+        {"HUP", SIGHUP},
+        {"INT", SIGINT},
+        {"QUIT", SIGQUIT},
+        {"ILL", SIGILL},
+        {"TRAP", SIGTRAP},
+        {"ABRT", SIGABRT},
+        {"IOT", SIGIOT},
+        {"BUS", SIGBUS},
+        {"FPE", SIGFPE},
+        {"KILL", SIGKILL},
+        {"USR1", SIGUSR1},
+        {"SEGV", SIGSEGV},
+        {"USR2", SIGUSR2},
+        {"PIPE", SIGPIPE},
+        {"ALRM", SIGALRM},
+        {"TERM", SIGTERM},
+        {"CHLD", SIGCHLD},
+        {"CONT", SIGCONT},
+        {"STOP", SIGSTOP},
+        {"TSTP", SIGTSTP},
+        {"TTIN", SIGTTIN},
+        {"TTOU", SIGTTOU},
+        {"URG", SIGURG},
+        {"XCPU", SIGXCPU},
+        {"XFSZ", SIGXFSZ},
+        {"VTALRM", SIGVTALRM},
+        {"PROF", SIGPROF},
+        {"WINCH", SIGWINCH},
+        {"IO", SIGIO},
+        {"SYS", SIGSYS},
+/senales que no hay en todas partes
+#ifdef SIGPOLL
+        {"POLL", SIGPOLL},
+#endif
+#ifdef SIGPWR
+        {"PWR", SIGPWR},
+#endif
+#ifdef SIGEMT
+        {"EMT", SIGEMT},
+#endif
+#ifdef SIGINFO
+        {"INFO", SIGINFO},
+#endif
+#ifdef SIGSTKFLT
+        {"STKFLT", SIGSTKFLT},
+#endif
+#ifdef SIGCLD
+        {"CLD", SIGCLD},
+#endif
+#ifdef SIGLOST
+        {"LOST", SIGLOST},
+#endif
+#ifdef SIGCANCEL
+        {"CANCEL", SIGCANCEL},
+#endif
+#ifdef SIGTHAW
+        {"THAW", SIGTHAW},
+#endif
+#ifdef SIGFREEZE
+        {"FREEZE", SIGFREEZE},
+#endif
+#ifdef SIGLWP
+        {"LWP", SIGLWP},
+#endif
+#ifdef SIGWAITING
+        {"WAITING", SIGWAITING},
+#endif
+        {NULL,-1},
+};    /fin array sigstrnum
+
+
+int ValorSenal(char * sen)  /devuelve el numero de senial a partir del nombre
 {
-    struct stat s;
-    ssize_t  n;
-    int df,aux;
-
-    if (stat (f,&s)==-1 || (df=open(f,O_RDONLY))==-1)
-        return -1;
-    if (cont==-1)   /* si pasamos -1 como bytes a leer lo leemos entero
-        cont=s.st_size;
-    if ((n=read(df,p,cont))==-1){
-        aux=errno;
-        close(df);
-        errno=aux;
-        return -1;
-    }
-    close (df);
-    return n;
-}
-
-void CmdRead (char *ar[])
-{
-    void *p;
-    size_t cont=-1;  /* -1 indica leer todo el fichero
-    ssize_t n;
-    if (ar[0]==NULL || ar[1]==NULL){
-        printf ("faltan parametros\n");
-        return;
-    }
-    p=cadtop(ar[1]);  /*convertimos de cadena a puntero
-    if (ar[2]!=NULL)
-        cont=(size_t) atoll(ar[2]);
-
-    if ((n=LeerFichero(ar[0],p,cont))==-1)
-        perror ("Imposible leer fichero");
-    else
-        printf ("leidos %lld bytes de %s en %p\n",(long long) n,ar[0],p);
+    int i;
+    for (i=0; sigstrnum[i].nombre!=NULL; i++)
+        if (!strcmp(sen, sigstrnum[i].nombre))
+            return sigstrnum[i].senal;
+    return -1;
 }
 
 
-
-
-
-void Do_MemPmap (void) /*sin argumentos
-{ pid_t pid;       /*hace el pmap (o equivalente) del proceso actual
-    char elpid[32];
-    char *argv[4]={"pmap",elpid,NULL};
-
-    sprintf (elpid,"%d", (int) getpid());
-    if ((pid=fork())==-1){
-        perror ("Imposible crear proceso");
-        return;
-    }
-    if (pid==0){ /*proceso hijo
-        if (execvp(argv[0],argv)==-1)
-            perror("cannot execute pmap (linux, solaris)");
-
-        argv[0]="vmmap"; argv[1]="-interleave"; argv[2]=elpid;argv[3]=NULL;
-        if (execvp(argv[0],argv)==-1) /*probamos vmmap Mac-OS
-            perror("cannot execute vmmap (Mac-OS)");
-
-        argv[0]="procstat"; argv[1]="vm"; argv[2]=elpid; argv[3]=NULL;
-        if (execvp(argv[0],argv)==-1)/*No hay pmap, probamos procstat FreeBSD
-            perror("cannot execute procstat (FreeBSD)");
-
-        argv[0]="procmap",argv[1]=elpid;argv[2]=NULL;
-        if (execvp(argv[0],argv)==-1)  /*probamos procmap OpenBSD
-            perror("cannot execute procmap (OpenBSD)");
-
-        exit(1);
-    }
-    waitpid (pid,NULL,0);
-}*/
+char *NombreSenal(int sen)  /devuelve el nombre senal a partir de la senal
+{			/ para sitios donde no hay sig2str
+    int i;
+    for (i=0; sigstrnum[i].nombre!=NULL; i++)
+        if (sen==sigstrnum[i].senal)
+            return sigstrnum[i].nombre;
+    return ("SIGUNKNOWN");
+}
+*/
 
 
 
